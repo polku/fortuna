26dedfea6c5ff8f151e6e2a86a72e56d5a58117b
Jonathan Maurice <jonathanmaurice539@gmail.com>
2025-07-01 17:58:54 +0200

    Fix navigation stack index error

--- lib/main.dart
+++ lib/main.dart
@@ -27,16 +27,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
-  final List<Widget> _pages = const [
-    PortfolioPage(),
-    BuyOperationPage(),
-    OperationsListPage(),
+  final List<Widget> _pages = [
+    const PortfolioPage(),
+    const BuyOperationPage(),
+    const OperationsListPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) => setState(() => _currentIndex = index),
